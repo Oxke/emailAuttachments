aa45ab0e970c4cd44d858fdf3a6696c4c90c136b
Oxke <oseaetobia@gmail.com>
2019-04-28 18:49:54 +0200

    Aggiunta una sezione più user-friendly "Settings"

--- settings.py
+++ settings.py
@@ -30,9 +30,8 @@ from pprint import pprint
 from shutil import rmtree
 
 from crypto import *
-from main import including_root  # NOTE: I don't know why, it runs only
-
-#  if you start the program from main
+from main import including_root     # NOTE: I don't know why, it runs only
+                                    #  if you start the program from main
 
 fin_cf = glob(including_root('config_folder\\*.eaa'))
 fin_dc = glob(including_root('config_folder\\data_config\\*.eaa'))
